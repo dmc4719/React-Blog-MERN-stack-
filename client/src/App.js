8d2149321330570e918f3d53c9ac20f796e8b7fb
dmc4719 <dmc4719@gmail.com>
2020-03-09 10:52:21 +0600

    new changes

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,8 @@ import Login from './components/Login'
 import Register from './components/Register'
 import Landing from './pages/Landing'
 import Posts from './pages/Blog/View_Posts'
-import createPosts from './pages/Blog/Create_Posts'
-import  adminLogin  from './components/adminLogin';
+import CreatePosts from './pages/Blog/Create_Posts'
+import  AdminLogin  from './components/adminLogin';
 import AdminHome  from './pages/AdminHome';
 import View_Single from './pages/Blog/View_Single';
 import Footer from './components/UI/Footer';
@@ -28,13 +28,13 @@ function App() {
     <div className="wrapper" >
       <Navbar/>
       <Switch>
-      <Route path='/admin' exact component={adminLogin}/>
+      <Route path='/admin' exact component={AdminLogin}/>
         <Route path='/admin/home' exact component={AdminHome}/>
         <Route path='/' exact component={Landing} />
         <Route path='/login' component={Login} />
         <Route path='/register' component={Register}/>
         <Route path='/posts' exact component={Posts}/>
-        <Route path='/posts/create_post' component={createPosts}/>
+        <Route path='/posts/create_post' component={CreatePosts}/>
         <Route path='/posts/:post_id' exact component={View_Single} />
         <Route path='/posts/update/:post_id' exact  component={Update_Post} />
       </Switch>
